2b7235053842c73edc9536806b30aa873dea9a92
osoriproduction <heinekenberg16@gmail.com>
2023-01-26 00:29:46 +0900

    어카운트 뽑는거 만들자.

---
+++ link_blockchain_scanner_account_hunter_v1.py
@@ -0,0 +1,185 @@
+from os import times
+from numpy import concatenate
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from bs4 import BeautifulSoup
+import time
+import telegram
+import asyncio
+
+import pandas as pd 
+import traceback
+import sys
+
+path_csv_file_name_input = "DB_link_blockchain_scanner_block_watcher_v1_52650000.csv"
+path_csv_file_name_account_list = "DB_account_list.csv" 
+
+
+def get_time():
+    timeList = time.localtime()
+    timeStr = timeList[0]*10000000000+timeList[1]*100000000+timeList[2]*1000000+timeList[3]*10000+timeList[4]*100+timeList[5]
+    return timeStr
+
+
+def trim_space(str_input):
+    """str의 공백을 제거하여 반환"""
+
+    return str_input.replace(' ', '')
+    
+
+
+def get_txhash_from_block_if_exist(block_height_input): 
+    """
+    block_number의 핀시아 블록을 서칭하여, 트랜잭션이 있을 경우, 해당 해쉬값을 string 형태로 반환함. 없을경우 빈 str을 반환함. 
+    """
+
+    block_height = str(block_height_input)
+    print("\n----------------------------------------------------")
+    print("block seraching started.....", block_height)
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1200')
+    options.add_argument("disable-gpu")
+        
+    driver = webdriver.Chrome('chromedriver',options=options ) 
+
+    target_url = 'https://scan.blockchain.line.me/Finschia%20Mainnet/blocks/' + block_height
+    print('target_url is.. ',target_url)
+
+    driver.get(target_url)
+    time.sleep(1)
+    # driver.implicitly_wait(5)
+
+    # element = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[3]/div[3]/div[2]/div/div/div[2]/div/div[2]/div/div/div/div/span[3]')))
+
+    html = driver.page_source        
+    soup = BeautifulSoup(html, 'html.parser')
+
+
+    # null_str = '<tbody role="rowgroup"></tbody>' #비어있는 tbody의 경우, [0]이 왼쪽과 같은 str을 반환함. 
+
+    tx_info =  soup.select("#app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(3) > div > div > table > tbody")
+    # print("tx_info: ",tx_info)
+    # print("tx_info[0]: ", tx_info[0])
+    # print(type(tx_info[0]))
+
+
+    if tx_info == None or len(tx_info) == 0:
+        print("Failed to loading page information..")
+        return (block_height_input, "FAILED")
+
+
+    if tx_info[0].text != "":
+        tx_info_detail = soup.select("#app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(3) > div > div > table > tbody > tr > td > a")
+        
+        # print("tx_info_detail[0].text: ",tx_info_detail[0].text)
+        
+        tx_hash_return = trim_space(tx_info_detail[0].text)
+        
+
+    else:        
+        tx_hash_return = "NA"
+        print("this block has no TX")
+
+    driver.close()
+    return (block_height_input, tx_hash_return)
+
+
+
+
+"""BOT에게 메시지 보내는 비동기 함수"""
+async def do_work_bot(message_to_send):
+    
+    my_token = "2062225545:AAGytzWEbs7_dzQK2aPV5FXjQCG5ucWq8uc" 
+    my_chat_id = 2031803571    
+    bot = telegram.Bot(token = my_token)
+    
+    await bot.send_message(chat_id=my_chat_id, text= message_to_send)
+    
+
+
+
+
+
+if __name__ == "__main__":
+    
+    # asyncio.run(do_work_bot("link wallet tracing program has been started"))
+
+    #finschia 시작: "51775519" 2022/12/22
+    
+    #52660132 - 트랜잭션 1개 있는 블록
+    #52660175 - 트랜잭션 없는 블록
+
+    
+    
+    #block~tx list를 가져온다.
+    try:
+        df_info_list = pd.read_csv(path_csv_file_name_input, index_col =0 )
+
+    except:
+        print("wrong input filed...")
+        sys.exit()
+
+
+    #account list를 생성하거나 로딩한다.
+    try:
+        df_account_list = pd.read_csv(path_csv_file_name_account_list, index_col =0 )
+        df_account_list.drop(labels=['START_LINE'],errors='ignore', inplace=True)        
+
+    except:
+        temp_dic = { "START_LINE" : { 'account' : '' }}  #잔액 조회는 다른 스크립트로 짠다.
+        df_account_list =  pd.DataFrame.from_dict(temp_dic, orient ='index')
+
+
+
+
+    try:
+        
+        df_tx_hashs = df_info_list.loc[df_info_list['tx_hash'].isnull() == False ]       
+                
+        print(df_tx_hashs)        
+        
+        
+        
+        for i in df_tx_hashs:  
+            
+            #트랜잭션을 돌면서, 셀레니움으로 어카운트를 추출하여 리스트에 넣어서 파일로 떨군다.(저장까지)
+            #단, 중복되는 어카운트의 경우는 어카운트 리스트에 넣지 않아야 하겠다.
+            
+            
+            # if (df_info_list['block_hash'] == i).any() == False  :
+                
+            #     temp_tx_pair = get_txhash_from_block_if_exist(i)
+            #     print(temp_tx_pair) 
+                
+            #     df_info_list.loc[get_time()] = {'block_hash' : temp_tx_pair[0], 'tx_hash':temp_tx_pair[1] }  
+            #     df_info_list.to_csv(path_csv_file_name)
+
+            # else:
+            #     print ("the block was already recorded")
+
+
+
+
+        # df_info_list.to_csv(path_csv_file_name)
+
+
+
+
+    except Exception as e:
+        trace_back = traceback.format_exc()
+        message = str(e)+ " " + str(trace_back)
+        print (message)
+        
+        # asyncio.run(do_work_bot("line wallet tracing program has been terminated by error"))
+
+
+
+
+
+
+
