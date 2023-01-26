cc2249982357b3844a292ea80225e581f1cfd454
osoriproduction <heinekenberg16@gmail.com>
2023-01-26 23:05:07 +0900

    account_hunter_v1 완성한듯!?

--- link_blockchain_scanner_account_hunter_v1.py
+++ link_blockchain_scanner_account_hunter_v1.py
@@ -15,7 +15,7 @@ import traceback
 import sys
 
 path_csv_file_name_input = "DB_link_blockchain_scanner_block_watcher_v1_52650000.csv"
-path_csv_file_name_account_list = "DB_account_list.csv" 
+path_csv_file_name_account_list = "DB_account_list_v1_52650000.csv" 
 
 
 def get_time():
@@ -92,8 +92,8 @@ def trim_space(str_input):
 def get_accounts_from_tx_hash(tx_hash):
     """트랜잭션 해쉬로부터, from, to address를 뽑는다."""
     
+    # tx_hash = str(tx_hash)
     
-    return_account_list = []
     
     print("\n----------------------------------------------------")
     print("tx_hash seraching started.....", tx_hash)
@@ -109,56 +109,62 @@ def get_accounts_from_tx_hash(tx_hash):
 
     driver.get(target_url)
     time.sleep(1)
-    # driver.implicitly_wait(5)
-
-    # element = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[3]/div[3]/div[2]/div/div/div[2]/div/div[2]/div/div/div/div/span[3]')))
 
     html = driver.page_source        
     soup = BeautifulSoup(html, 'html.parser')
 
+    # __BVID__127 : #app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(2) > div > table
+    # __BVID__135 : #__BVID__127 > tbody > tr:nth-child(2) > td.overflow-hidden > div > table
+                    #app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(2) > div > table > tbody > tr:nth-child(2) > td.overflow-hidden > div > table
+    # from_address: #__BVID__135 > tbody > tr:nth-child(1) > td:nth-child(2) > div > span
+    # to_address: #__BVID__135 > tbody > tr:nth-child(2) > td:nth-child(2) > div > span
 
-    tx_type = soup.select("#app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(2) > div > div")
     
+    tx_type = soup.select("#app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > div")
+    str_type = trim_space(tx_type[0].text)
     
-    tx_info_1 =  soup.select("#app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(3) > div > div > table > tbody")
-    # print("tx_info: ",tx_info)
-    # print("tx_info[0]: ", tx_info[0])
-    # print(type(tx_info[0]))
+    print( str_type)
 
+    TYPE_MSG_SEND = 'cosmos-sdk/MsgSend'
+    TYPE_MSG_CONTRACT = 'wasm/MsgExecuteContract'
 
-    if tx_info == None or len(tx_info) == 0:
+    if str_type == TYPE_MSG_SEND:  #ex) 7C271E8B8C9A59C37719DF4DC1025DF6D656660A800C220223450E824E57B093
+        from_address_obj = soup.select("#app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(2) > div > table > tbody > tr:nth-child(2) > td.overflow-hidden > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > div")
+        to_address_obj = soup.select("#app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(2) > div > table > tbody > tr:nth-child(2) > td.overflow-hidden > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > div")
+        # print( from_address_obj[0].text)
+        # print( to_address_obj[0].text)
+
+        from_address = str(from_address_obj[0].text)
+        to_address = str(to_address_obj[0].text)
+        
+    elif str_type == TYPE_MSG_CONTRACT: #ex) 1E493FCC7B67E0A7DB2BF73220C130070B7F7477D341E6AB3CFEB2F34D46DADF
+        
+        from_address_obj = soup.select("#app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(2) > div > table > tbody > tr:nth-child(2) > td.overflow-hidden > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > div")
+        to_address_obj = soup.select("#app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(2) > div > table > tbody > tr:nth-child(2) > td.overflow-hidden > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > div")
+
+        from_address = str(from_address_obj[0].text)
+        to_address = str(to_address_obj[0].text)
+         
+        
+    else:
         print("Failed to loading page information..")
-        return (block_height_input, "FAILED")
-
-
-    if tx_info[0].text != "":
-        tx_info_detail = soup.select("#app > div > div.app-content.content > div.content-wrapper > div.content-body > div > div:nth-child(3) > div > div > table > tbody > tr > td > a")
+        from_address =""
+        to_address =""
         
-        # print("tx_info_detail[0].text: ",tx_info_detail[0].text)
-        
-        tx_hash_return = trim_space(tx_info_detail[0].text)
-        
-
-    else:        
-        tx_hash_return = "NA"
-        print("this block has no TX")
 
     driver.close()
-    return (block_height_input, tx_hash_return)
+     
+     
+     
     
+    return_account_list = []
     
-    
-    
-    return_account_list.append('aaa')
-    
-    return_account_list.append('bbb')
+    if(from_address != ""):        
+        return_account_list.append(from_address)        
+        
+    if(to_address != ""):
+        return_account_list.append(to_address)
 
-    # return_account_list.append(tx_hash[:5])
-    
-    # return_account_list.append(tx_hash[4:])
-
-    
-    
     return return_account_list
 
 
@@ -183,11 +189,6 @@ if __name__ == "__main__":
     # asyncio.run(do_work_bot("link wallet tracing program has been started"))
 
     #finschia 시작: "51775519" 2022/12/22
-    
-    #52660132 - 트랜잭션 1개 있는 블록
-    #52660175 - 트랜잭션 없는 블록
-
-    
     
     #block~tx list를 가져온다.
     try:
@@ -220,6 +221,7 @@ if __name__ == "__main__":
                                                   
             if df_info_list['tx_hash'].iloc[row] == 'nan' :
                 print("not valid block-TX type (row)")
+                df_info_list.iloc[row, 2] += 1
                 continue
                         
             #트랜잭션을 돌면서, 셀레니움으로 어카운트를 추출하여 리스트에 넣어서 파일로 떨군다.(저장까지)
@@ -230,7 +232,7 @@ if __name__ == "__main__":
             
             else:
                 df_info_list.iloc[row, 2] += 1  
-                temp_account_list = get_accounts_from_tx_hash(df_info_list['search_count'].iloc[row])
+                temp_account_list = get_accounts_from_tx_hash(df_info_list['tx_hash'].iloc[row])
                                 
                 for i in range(0, len(temp_account_list)):                    
                     
