ca6fcdb816a7edefe020e79027a997124da27e16
LAPTOP-MP3CS2KV\heine <heinekenberg16@gmail.com>
2023-01-28 01:15:01 +0900

    print 주석처리, 프로세스 종료되면 알림 오게

--- link_blockchain_scanner_block_parser_v2.py
+++ link_blockchain_scanner_block_parser_v2.py
@@ -16,17 +16,14 @@ from functools import partial
 # from pathos.multiprocessing import ProcessingPool as Pool #pip install pathos
 import multiprocessing
 
-path_csv_file_name_frame = "DB_link_blockchain_scanner_block_parser_v1"
+path_csv_file_name_frame = "DB\DB_link_blockchain_scanner_block_parser_v1"
 
 #finschia 시작: "51775519" 2022/12/22
 
 #sample
 #52660132 - 트랜잭션 1개 있는 블록
 #52660175 - 트랜잭션 없는 블록
-start_block = 52550000
-# end_block = 52599999
-# path_csv_file_name = path_csv_file_name_frame + "_"+str(start_block) +"_"+str(end_block) + ".csv"
-
+start_block = 52500000
 
 
 def get_time():
@@ -59,8 +56,8 @@ def get_txhash_from_block_if_exist(block_height_input):
     """
 
     block_height = str(block_height_input)
-    print("\n----------------------------------------------------")
-    print("block seraching started.....", block_height)
+    # print("\n----------------------------------------------------")
+    # print("block seraching started.....", block_height)
 
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
@@ -70,7 +67,7 @@ def get_txhash_from_block_if_exist(block_height_input):
     driver = webdriver.Chrome('chromedriver',options=options ) 
 
     target_url = 'https://scan.blockchain.line.me/Finschia%20Mainnet/blocks/' + block_height
-    print('target_url is.. ',target_url)
+    # print('target_url is.. ',target_url)
 
     driver.get(target_url)
     time.sleep(1)
@@ -83,7 +80,7 @@ def get_txhash_from_block_if_exist(block_height_input):
 
 
     if tx_info == None or len(tx_info) == 0:
-        print("Failed to loading page information..")
+        # print("Failed to loading page information..")
         return (block_height_input, "FAILED")
 
 
@@ -94,7 +91,7 @@ def get_txhash_from_block_if_exist(block_height_input):
 
     else:        
         tx_hash_return = "NA"
-        print("this block has no TX")
+        # print("this block has no TX")
 
     driver.close()
     
@@ -108,7 +105,7 @@ def do_multi( code):
     """멀티스레드 실행."""    
     
     print("do_multi.. ", code)
-    #50000개 / 5프로세스 = 10000.   5프로세스의 경우, 10000개씩 끊어서 프로세싱 돌리기.     
+    #50000개 / 5프로세스 = 10000.   무조건 10000개씩 끊어서 파일 간격 맞춰서 프로세싱 돌리기.     
     
     temp_start_block = start_block + code*10000
     temp_end_block = temp_start_block + 9999
@@ -123,25 +120,26 @@ def do_multi( code):
         df_info_list =  pd.DataFrame.from_dict(temp_dic, orient ='index')
 
 
-    print(df_info_list)
+    # print(df_info_list)
 
     
     
     for i in range(temp_start_block,temp_end_block):
              
-        print("multithreade.. node .. ", code, ': ',i)
+        # print("multithreade.. node .. ", code, ': ',i)
 
         if (df_info_list['block_hash'] == i).any() == False  :
             
             temp_tx_pair = get_txhash_from_block_if_exist(i)
 
-            print(code, temp_tx_pair) 
+            # print(code, temp_tx_pair) 
         
             df_info_list.loc[get_time()] = {'block_hash' : temp_tx_pair[0], 'tx_hash':temp_tx_pair[1] ,'search_count':0 }  
             df_info_list.to_csv(temp_path_csv_file_name)
 
         else:
-            print ("the block was already recorded")
+            pass
+            # print ("the block was already recorded")
 
 
     df_info_list.drop(labels=['START_LINE'],errors='ignore', inplace=True) 
@@ -155,21 +153,21 @@ def do_multi( code):
 
 if __name__ == "__main__":
 
+    asyncio.run(do_work_bot("block_parser_program has been started"))
 
     code_list = [0,1,2,3,4]
     print ('--- start _multiprocessing')
 
 
-    # # # cpu 갯수 확인
-    # cpu_count = multiprocessing.cpu_count()
-    # print ('--- cpu_count ', cpu_count)
+    # cpu 갯수 확인
+    cpu_count = multiprocessing.cpu_count()
+    print ('--- cpu_count ', cpu_count)
 
     # cpu 수 결정
     pool = multiprocessing.Pool(processes= 5)
     
      # 실행 함수, 넘겨줄 파라미터
     
-    # func= partial(do_multi,  df_info_list)
     pool.map(do_multi, code_list)
 
     # 모든 프로세스 종료까지 기다림
@@ -178,6 +176,7 @@ if __name__ == "__main__":
     
     print ('--- end _multiprocessing')
 
+    asyncio.run(do_work_bot("block_parser_program has been completed"))
 
 
 
